f858d6f12ab2e4cf142274a6bf84921291b3de83
asad0i <mda17059@gmail.com>
2022-04-26 13:38:32 +0600

    Show demo
    asdfdf

--- js/main.js
+++ js/main.js
@@ -1,168 +1,71 @@
 
-    $(window).on("load", function() {
-        // Preloadert/
-$(".preloader").fadeOut("slow");
-});
+
+// Set the date we're counting down to
+var countDownDate = new Date("May 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-2").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-3").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-4").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+
+    document.getElementById("demo-5").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-6").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-7").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
 
 
-$(document).ready (function() {
-    // navbar shrink
-    $(window).on("scroll", function(){
-        if($(this).scrollTop() > 90){
-            $(".navbar").addClass("navbar-shrink")
-        }
-        else{
-            $(".navbar").removeClass("navbar-shrink")
-        }
-    })
-    
-    //video Popup
-const videoSrc = $("#player-01").attr("Src");
+    document.getElementById("demo-8").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-9").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-10").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
 
-$(".video-play-btn, .video-popup").on("click", function(){
-    if($(".video-popup").hasClass("open")){
-        $(".video-popup").removeClass("open");
-        $("#player-01").attr("Src","");
+    document.getElementById("demo-11").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-12").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+    document.getElementById("demo-13").innerHTML = days + ": " + hours + ":"
+        + minutes + ":" + seconds + " ";
+
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("demo-2").innerHTML = "EXPIRED";
+        document.getElementById("demo-3").innerHTML = "EXPIRED";
+        document.getElementById("demo-4").innerHTML = "EXPIRED";
+        document.getElementById("demo-5").innerHTML = "EXPIRED";
+        document.getElementById("demo-6").innerHTML = "EXPIRED";
+        document.getElementById("demo-7").innerHTML = "EXPIRED";
+        document.getElementById("demo-8").innerHTML = "EXPIRED";
+        document.getElementById("demo-9").innerHTML = "EXPIRED";
+        document.getElementById("demo-10").innerHTML = "EXPIRED";
+        document.getElementById("demo-11").innerHTML = "EXPIRED";
+        document.getElementById("demo-12").innerHTML = "EXPIRED";
+        document.getElementById("demo-13").innerHTML = "EXPIRED";
     }
-     else{
-        $(".video-popup").addClass("open");
-        if( $("#player-01").attr("Src")==''){
-        $("#player-01").attr("Src",videoSrc);
-        } 
-    }
-
-
-
-});
-//scroll
-$.scrollIt({
-    topOffset: -50
-});
-function toggleTheme(){
-    if(localStorage.getItem("shala-theme") !== null){
-        if(localStorage.getItem("shala-theme") === "dark" ){
-            $("body").addClass("dark");
-        }
-        else{
-            $("body").removeClass("dark");
-        }
-    }
-    updateIcon();
-}
-toggleTheme();
-$(".toggle-theme").on("click",function(){
-    $("body").toggleClass("dark")
-    if($("body").hasClass("dark")){
-        localStorage.setItem("shala-theme","dark");
-    }
-    else{
-        localStorage.setItem("shala-theme","light");
-    }
-    updateIcon();
-});
-
-function updateIcon(){
-    if($("body").hasClass("dark")){
-         $(".toggle-theme i").removeClass("fa-moon");
-         $(".toggle-theme i").addClass("fa-sun");
-    }
-    else{
-        $(".toggle-theme i").removeClass("fa-sun");
-        $(".toggle-theme i").addClass("fa-moon");
-    }
-}
-
-});
-
-
-// owl carasol
-
-$('.features-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            
-        },
-        600:{
-            items:2,
-            
-        },
-        1000:{
-            items:3,
-            
-        }
-    }
-
-    
-});
-//app screenshorts carousel
-$('.sreenshorts-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-            
-        },
-        600:{
-            items:2
-            
-        },
-        1000:{
-            items:4
-            
-        }
-    }
-
-    
-});
-
-// Tesmonial  carousel
-$('.testimonials-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-            
-        },
-        600:{
-            items:2
-            
-        },
-        1000:{
-            items:3
-            
-        }
-    }
-
-    
-});
-
-// Team Carousel
-$('.team-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1
-            
-        },
-        600:{
-            items:2
-            
-        },
-        1000:{
-            items:3
-            
-        }
-    }
-
-    
-});
+}, 1000);
